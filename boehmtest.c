5a773901cbd97f58101f84d9f34cf3f0ee84485c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-08-03 01:03:55 -0400

    Improved code using Splint

--- boehmtest.c
+++ boehmtest.c
@@ -12,7 +12,28 @@
 
 	Requires Boem GC
 	http://www.hpl.hp.com/personal/Hans_Boehm/gc/
+
+	Splint annotations by Guillermo
+	http://stackoverflow.com/questions/6903272#6904955
 */
+
+/*@shared@*/ /*@null@*/ /*@out@*/ static void * SPLINT_GC_MALLOC(size_t size) /*@*/ {
+	/*@-onlytrans@*/
+	return GC_MALLOC(size);
+	/*@=onlytrans@*/
+}
+
+/*@shared@*/ /*@null@*/ /*@out@*/ static void * SPLINT_GC_MALLOC_ATOMIC(size_t size) /*@*/ {
+	/*@-onlytrans@*/
+	return GC_MALLOC_ATOMIC(size);
+	/*@=onlytrans@*/
+}
+
+/*@shared@*/ /*@null@*/ /*@out@*/ static void * SPLINT_GC_REALLOC(void *ptr, size_t size) /*@*/ {
+	/*@-onlytrans@*/
+	return GC_REALLOC(ptr, size);
+	/*@=onlytrans@*/
+}
 
 int main(void) {
 	int i;
@@ -20,14 +41,18 @@ int main(void) {
 	GC_INIT();
 
 	for (i = 0; i < 10000000; i++) {
-		int **p = GC_MALLOC(sizeof(int *));
-		int *q = GC_MALLOC_ATOMIC(sizeof(int));
+		int **p = SPLINT_GC_MALLOC(sizeof(int *));
+		int *q = SPLINT_GC_MALLOC_ATOMIC(sizeof(int));
 
-		assert(*p == 0);
-		*p = GC_REALLOC(q, 2 * sizeof(int));
+		if (p != NULL && q != NULL) {
+			assert(*p == 0);
+			*p = SPLINT_GC_REALLOC(q, 2 * sizeof(int));
 
-		if (i % 100000 == 0) {
-			printf("Heap size = %zu\n", GC_get_heap_size());
+			if (i % 100000 == 0) {
+				printf("Heap size = %u\n", (unsigned int) GC_get_heap_size());
+			}
 		}
 	}
+
+	return 0;
 }
